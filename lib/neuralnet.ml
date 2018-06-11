41a3858aac1f24d06698af646f397b7c1063f7c4
Jeremy Lacomis <jlacomis@cmu.edu>
2018-06-11 10:31:17 -0400

    Initialize Python 3.6 and dynet

--- lib/neuralnet.ml
+++ lib/neuralnet.ml
@@ -1,3 +1,13 @@
-Py.initialize ()
+let initialize =
+  let version = Some 3 in
+  let minor = Some 6 in
+  Py.initialize ?version:version ?minor:minor ()
 
-let test_python () = Py.Run.simple_string "print('Hello, world!')"
+let test_dynet () =
+  let import_sys =
+    "import sys\n\
+     if not hasattr(sys, 'argv'):\n\
+     \tsys.argv = ['']"
+  in
+  ignore (Py.Run.simple_string import_sys);
+  Py.Run.simple_string "import dynet"
