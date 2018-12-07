60001a33597e41f0b7ab83392ae8a2c0919db8c3
Jeremy Lacomis <jlacomis@cmu.edu>
2018-12-07 14:02:22 -0500

    Collect types

--- bin/collect.ml
+++ bin/collect.ml
@@ -1,9 +1,24 @@
 open Cil
-open Typesig
+open Utils
+
+module TS = Typesig
 
 let main() =
   initCIL ();
-  add_base_types global_signatures;
+  TS.add_base_types TS.global_signatures;
+  let fnames =
+    match Array.to_list Sys.argv with
+    | [ _ ] | [] -> failwith "no input files"
+    | _ :: files -> files
+  in
+  Printf.printf "%d input files:\n" (List.length fnames);
+  let process_file fname =
+    Printf.printf "Processing %s\n%!" fname;
+    let parsed = parse_one_file fname in
+    TS.collect_types parsed;
+  in
+  List.iter process_file fnames;
+  TS.print_types TS.global_signatures;
 ;;
 
 main ();
